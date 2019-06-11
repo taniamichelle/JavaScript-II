61172f8a0883025ebb02728b82c695e804ccef5a
tania <tanislavens@yahoo.com>
2019-06-11 15:20:02 -0400

    WIP

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const addOne = () => {
+  let addOne = 0;
+  return () => {
+    addOne += addOne;
+    return addOne;
+  }
+};
+
+console.log(addOne());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
